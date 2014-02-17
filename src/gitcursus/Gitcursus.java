b199e6379065d5e67dc0bb66117da5b85f7ac743
Thomas Deserranno <thomas.deserranno@gmail.com>
2014-02-17 09:28:19 +0100

    hallo iedereen

--- src/gitcursus/Gitcursus.java
+++ src/gitcursus/Gitcursus.java
@@ -17,7 +17,7 @@ public class Gitcursus {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Bonjour");
+        System.out.println("Hallo iedereen");
     }
     
 }
